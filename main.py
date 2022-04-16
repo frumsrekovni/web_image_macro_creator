aac4684069620fd771168085a50a4ad75557a741
frumsrekovni <joshua.backstrom@gmail.com>
2022-04-16 22:07:17 +0200

    Latest chromdriver is now installed at start. Added an option to choose between older and recent uploaded images. Fixed the recent change in source html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -19,11 +20,13 @@ def main():
     ############### FETCH USER ARGUMENTS ###############
 
     google_search_term = input("Enter the Google search term: ")
+    how_recent = input("Recent[1] or Past year[2]: ")
     top_text = input("Top text: ")
     bottom_text = input("Bottom text: ")
 
     ############### OPEN GOOGLE ###############
-    ser = Service("C:\Program Files (x86)\chromedriver.exe")
+    # ser = Service("C:\Program Files (x86)\chromedriver.exe")
+    ser = Service(ChromeDriverManager().install()) #Need up to date Chromedriver
     options = webdriver.ChromeOptions()
     options.add_argument("--incognito")
     driver = webdriver.Chrome(service=ser,options=options)
@@ -35,11 +38,16 @@ def main():
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='L2AGLb']"))).click()
     driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys(google_search_term + Keys.ENTER)
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='hdtb-msb']/div[1]/div/div[2]/a"))).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='yDmH0d']/div[2]/c-wiz/div[1]/div/div[1]/div[2]/div[2]/div/div"))).click()
+    
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='yDmH0d']/div[2]/c-wiz/div[1]/div/div[1]/div[2]/div[2]/div"))).click()
     element = driver.find_element(By.XPATH,"//*[@id='yDmH0d']/div[2]/c-wiz/div[2]/div[2]/c-wiz[1]/div/div/div[1]/div/div[4]/div")
     webdriver.ActionChains(driver).move_to_element(element).click(element ).perform()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='yDmH0d']/div[2]/c-wiz/div[2]/div[2]/c-wiz[1]/div/div/div[3]/div/a[2]"))).click()
+    if(how_recent == "1"):
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='yDmH0d']/div[2]/c-wiz/div[2]/div[2]/c-wiz[1]/div/div/div[3]/div/a[2]"))).click()
+    else:
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='yDmH0d']/div[2]/c-wiz/div[2]/div[2]/c-wiz[1]/div/div/div[3]/div/a[5]/div"))).click()
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"//*[@id='islrg']/div[1]/div[1]/a[1]"))).click()
+    
     time.sleep(5) # for some reason using driver.implicitly_wait() here doesnt work and it just downloads the thumbnail
     src = driver.find_element(By.XPATH, "//*[@id='Sva75c']/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img").get_attribute("src")
     urllib.request.urlretrieve(src, image_path)
